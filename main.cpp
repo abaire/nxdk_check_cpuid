292499b450b21b382aecbc712675d8c940b133ba
Erik Abair <erik.abair@gmail.com>
2021-10-28 15:33:08 -0700

    Adds all of the modes queried by xemu.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <hal/video.h>
 #include <hal/xbox.h>
-#include <pbkit/pbkit.h>
 #include <nxdk/mount.h>
+#include <pbkit/pbkit.h>
 #include <windows.h>
 
 #include <string>
@@ -9,9 +9,12 @@
 #define OUTPUT_FILE "E:\\cpuid.txt"
 
 static DWORD eax_values[] = {
-        0x0,        0x1,        0x2,        0x3,        0x4,
-        0x5,        0x80000000, 0x80000001, 0x80000002, 0x80000003,
-        0x80000004, 0x80000005, 0x80000006, 0x80000007,0x80000008,
+    0x0,        0x1,        0x2,        0x3,        0x4,        0x5,
+    0x06,       0x07,       0x09,       0x0A,       0x0B,       0x1F,
+    0x0D,       0x14,       0x40000000, 0x40000001, 0x80000000, 0x80000001,
+    0x80000002, 0x80000003, 0x80000004, 0x80000005, 0x80000006, 0x80000007,
+    0x80000008, 0x8000000A, 0x8000001D, 0x8000001E, 0xC0000000, 0xC0000001,
+    0xC0000002, 0xC0000003, 0xC0000004, 0x8000001F,
 };
 
 static void display_screen(const char *text) {
@@ -31,7 +34,8 @@ static void display_screen(const char *text) {
 }
 
 static BOOL dump_cpuid_results(const char *file_path) {
-  HANDLE file = CreateFile(file_path, GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+  HANDLE file = CreateFile(file_path, GENERIC_WRITE, FILE_SHARE_READ, nullptr,
+                           CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
   if (file == INVALID_HANDLE_VALUE) {
     DbgPrint("Failed to create output file.");
     return FALSE;
@@ -56,17 +60,18 @@ static BOOL dump_cpuid_results(const char *file_path) {
         "mov %%ebx, %[EBX]\n\t"
         "mov %%ecx, %[ECX]\n\t"
         "mov %%edx, %[EDX]\n\t"
-        : [EAX] "=rm" (EAX), [EBX] "=rm" (EBX), [ECX] "=rm" (ECX), [EDX] "=rm" (EDX)
-        : [mode_select] "r" (mode)
-        : "%eax", "%ebx", "%ecx", "%edx"
-        );
+        : [EAX] "=rm"(EAX), [EBX] "=rm"(EBX), [ECX] "=rm"(ECX), [EDX] "=rm"(EDX)
+        : [mode_select] "r"(mode)
+        : "%eax", "%ebx", "%ecx", "%edx");
 
-    snprintf(output_buffer, 255, "MODE=0x%08X, EAX=0x%08X, EBX=0x%08X, ECX=0x%08X, EDX=0x%08X\n", mode, EAX, EBX, ECX, EDX);
+    snprintf(output_buffer, 255,
+             "MODE=0x%08X, EAX=0x%08X, EBX=0x%08X, ECX=0x%08X, EDX=0x%08X\n",
+             mode, EAX, EBX, ECX, EDX);
     DbgPrint(output_buffer);
 
     DWORD buffer_len = strlen(output_buffer);
     DWORD bytes_written;
-    if( !WriteFile(file, output_buffer, buffer_len, &bytes_written, nullptr)) {
+    if (!WriteFile(file, output_buffer, buffer_len, &bytes_written, nullptr)) {
       snprintf(output_buffer, 255, "WriteFile failed: 0x%X", GetLastError());
       DbgPrint(output_buffer);
       ret = FALSE;
@@ -74,7 +79,8 @@ static BOOL dump_cpuid_results(const char *file_path) {
     }
 
     if (bytes_written != buffer_len) {
-      snprintf(output_buffer, 255, "Partial write failure: wrote %d of %d", bytes_written, buffer_len);
+      snprintf(output_buffer, 255, "Partial write failure: wrote %d of %d",
+               bytes_written, buffer_len);
       DbgPrint(output_buffer);
       ret = FALSE;
       goto cleanup;
@@ -95,7 +101,6 @@ int main() {
   }
   pb_show_front_screen();
 
-
   if (!nxMountDrive('E', R"(\Device\Harddisk0\Partition1)")) {
     DbgPrint("Failed to mount E:");
 
@@ -106,10 +111,12 @@ int main() {
 
   if (dump_cpuid_results(OUTPUT_FILE)) {
     display_screen(
-        "`cpuid` invocation results are in E:\\cpuid.txt\nSleeping 10 seconds and rebooting...");
+        "`cpuid` invocation results are in E:\\cpuid.txt\nSleeping 10 seconds "
+        "and rebooting...");
   } else {
     display_screen(
-        "Failed to dump `cpuid` invocation results.\nSleeping 10 seconds and rebooting...");
+        "Failed to dump `cpuid` invocation results.\nSleeping 10 seconds and "
+        "rebooting...");
   }
 
   Sleep(10 * 1000);
